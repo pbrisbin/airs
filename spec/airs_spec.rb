ac8511bdfd9a0748f4c1c17173110e05ed0af991
patrick brisbin <pbrisbin@gmail.com>
2013-03-05 11:46:35 -0500

    refactor spec

--- spec/airs_spec.rb
+++ spec/airs_spec.rb
@@ -2,29 +2,32 @@ require 'spec_helper'
 
 module Airs
   describe Main do
+    def run(patterns)
+      subject.run(patterns.join("\n"))
+    end
+
     before do
-      source = double("Next episode source")
-      source.stub(:todays_titles).and_return([
-        "Foo", "Biz", "Bar", "Buz", "Baz"
-      ])
+      subject.stub(:puts)
+
+      source = double("Next episode source",
+                      :todays_titles => %w( Foo Biz Bar Buz Baz ))
 
       NextEpisode.stub(:new).and_return(source)
-
-      subject.stub(:puts)
     end
 
     it "should push when shows are airing" do
-      push = double("Push", :send! => true)
+      push = double("Push")
+      push.should_receive(:send!)
 
       Push.should_receive(:new).with("Airs today: Foo, Bar, Baz").and_return(push)
 
-      subject.run(["foo", "ba.*"].join("\n"))
+      run %w( foo ba.* )
     end
 
     it "should not push when shows aren't airing" do
       Push.should_not_receive(:new)
 
-      subject.run(["flim", "flam"].join("\n"))
+      run %w( flim flam )
     end
   end
 end
